945e8addbd08e0ca0bc77157614cdc5bbbce6f43
pyladeepthi04 <pyladeepthi04@gmail.com>
2026-07-23 18:30:16 +0530

    Delete Spring Data JPA with Hibernate directory

--- Spring Data JPA with Hibernate/spring-data-jpa-handson/src/main/java/com/cognizant/repository/CountryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cognizant.repository;
-
-import com.cognizant.model.Country;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CountryRepository extends JpaRepository<Country, String> {
-
-}
